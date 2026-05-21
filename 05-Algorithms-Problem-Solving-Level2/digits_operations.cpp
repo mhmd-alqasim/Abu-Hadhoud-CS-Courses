b0f48b3ebc987aee10f2de361110d668bf71c14d
mhmd-alqasim <mhmd.alqasim@hotmail.com>
2026-05-21 20:53:08 +0300

    number operation problem 1-10

--- 05-Algorithms-Problem-Solving-Level2/digits_operations.cpp
+++ 05-Algorithms-Problem-Solving-Level2/digits_operations.cpp
@@ -47,12 +47,47 @@ int PrintReverse(int number)
     }
     return reverseNumber;
 }
+// 55956
+// 3
+int DigitFrequency(int number, int numberTarget)
+{
+    int Frequency = 0;
+    int reminder = 0;
+    while (number > 0)
+    {
+        reminder = number % 10;
+        number = number / 10;
+        if (reminder == numberTarget)
+        {
+            Frequency++;
+        }
+    }
+    return Frequency;
+}
+// 55956
+// 3
+void FrequencyForAllDigits(int Number)
+{
 
+    for (int i = 0; i < 10; i++)
+    {
+        int Digitfreq = 0;
+        Digitfreq = DigitFrequency(Number, i);
+        if (Digitfreq > 0)
+        {
+            cout << "Digit " << i << " Frequencey is " << Digitfreq << " Time(s).\n";
+        }
+    }
+}
 int main()
 {
     int number = ReadPositiveNumber("enter number ");
     Print_digits_in_reversed_order(number);
     cout << "sum digits = " << SumDigits(number) << endl;
     cout << "reverse number " << PrintReverse(number) << endl;
+    Print_digits_in_reversed_order(PrintReverse(number));
+
+    cout << DigitFrequency(number, ReadPositiveNumber("Please enter one digit to check")) << endl;
+    FrequencyForAllDigits(number);
     return 0;
 }

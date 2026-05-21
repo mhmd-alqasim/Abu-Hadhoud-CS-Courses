b71621bfeb67bd10663d322be3986db63a97c995
mhmd-alqasim <mhmd.alqasim@hotmail.com>
2026-05-21 15:08:24 +0300

    done the multiplication table

--- 05-Algorithms-Problem-Solving-Level2/multiplication_table.cpp
+++ 05-Algorithms-Problem-Solving-Level2/multiplication_table.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 using namespace std;
 
+void printHeaderTable()
+{
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "\t" << i;
+    }
+    cout << "\n______________________________________________________________________________________\n";
+}
+
+void PrintmultipicationTable()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i < 10)
+        {
+            cout << i << "  |";
+        }
+        else
+            cout << i << " |";
+
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << "\t" << i * j;
+        }
+        cout << endl;
+    }
+}
+void printAllTable()
+{
+    printHeaderTable();
+    PrintmultipicationTable();
+}
+
 int main()
 {
+    printAllTable();
     return 0;
 }
